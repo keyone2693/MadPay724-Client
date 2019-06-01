27ac1454a1b400ad4c80331840e5df9d773b57d0
keyone2693 <key.one72@gmail.com>
2019-06-01 19:07:54 +0430

    S06-E03

--- MadPay724-App/src/app/app.component.ts
+++ MadPay724-App/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+constructor(private http: HttpClient) {}
+
   title = 'MadPay724-App';
 }
