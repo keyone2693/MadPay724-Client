9781cacc02627fd614b45a17b8b73a2e6b8d0042
Key1-lab <key.one72@gmail.com>
2019-08-15 13:42:55 +0430

    S24-E17

--- MadPay724-Admin/src/app/components/panel/components/user/components/manage-ticket/components/list-ticket/list-ticket.component.ts
+++ MadPay724-Admin/src/app/components/panel/components/user/components/manage-ticket/components/list-ticket/list-ticket.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { NgxUiLoaderService } from 'ngx-ui-loader';
 import { NgxUiLoaderModule, NgxUiLoaderConfig, POSITION, SPINNER, PB_DIRECTION } from 'ngx-ui-loader';
+import { BehaviorSubject } from 'rxjs';
+import { TicketService } from 'src/app/Services/panel/user/ticket.service';
+import { AuthService } from 'src/app/Services/auth/auth.service';
 
 @Component({
   selector: 'app-list-ticket',
@@ -9,25 +12,38 @@ import { NgxUiLoaderModule, NgxUiLoaderConfig, POSITION, SPINNER, PB_DIRECTION }
 })
 export class ListTicketComponent implements OnInit {
   loaders: any;
-  constructor(private ngxService: NgxUiLoaderService) {
+  tickets = new BehaviorSubject([]);
+  lastKey = '';
+  finished = false;
+  constructor(private ngxService: NgxUiLoaderService, private ticketService: TicketService, private authService: AuthService) {
     this.loaders = {
-        pbColor: '#ff4444',
-        bgsColor: '#ff4444',
-        bgsPosition: POSITION.bottomCenter,
-        bgsSize: 70,
-        fgsPosition: POSITION.bottomCenter,
-        fgsSize: 70,
-        fgsColor: '#ff4444',
-        bgsType: SPINNER.threeBounce,
-        fgsType: SPINNER.threeBounce,
-        pbDirection: PB_DIRECTION.rightToLeft,
-        pbThickness: 3,
-        overlayColor: 'rgba(40,40,40,0)'
-      };
+      pbColor: '#ff4444',
+      bgsColor: '#ff4444',
+      bgsPosition: POSITION.bottomCenter,
+      bgsSize: 70,
+      fgsPosition: POSITION.bottomCenter,
+      fgsSize: 70,
+      fgsColor: '#ff4444',
+      bgsType: SPINNER.threeBounce,
+      fgsType: SPINNER.threeBounce,
+      pbDirection: PB_DIRECTION.rightToLeft,
+      pbThickness: 3,
+      overlayColor: 'rgba(40,40,40,0)'
+    };
   }
 
   ngOnInit() {
-    this.ngxService.startLoader('loader-01');
+    this.getTickets();
+    // this.ngxService.startLoader('loader-01');
+  }
+  onScroll() {
+    this.getTickets();
+  }
+  private getTickets(key?) {
+    if (this.finished) {
+      return;
+    }
+    this.ticketService.getTickets(this.authService.decodedToken.nameid)
   }
 
 }
