7a3fc93c1d6b713a9b9a9130f05327bb29980eb2
Key1-lab <key.one72@gmail.com>
2019-08-29 15:23:57 +0430

    S24-E21

--- MadPay724-Admin/src/app/components/panel/components/user/components/manage-ticket/components/list-ticket/components/create-form-ticket/create-form-ticket.component.ts
+++ MadPay724-Admin/src/app/components/panel/components/user/components/manage-ticket/components/list-ticket/components/create-form-ticket/create-form-ticket.component.ts
@@ -13,28 +13,47 @@ import { ToastrService } from 'ngx-toastr';
 })
 export class CreateFormTicketComponent implements OnInit {
   @Output() newTicket = new EventEmitter<Ticket>();
-  ticket: Ticket;
+  slectedFile: File = null;
+  selected = false;
+  fileName = '';
   constructor(private formBuilder: FormBuilder, private matdialogRef: MatDialogRef<CreateFormTicketComponent>,
               private ticketService: TicketService, private authService: AuthService,
               private alertService: ToastrService) { }
 
   ticketForm: FormGroup = this.formBuilder.group({
-    id: [],
     title: ['', [Validators.required, Validators.maxLength(50)]],
     department: ['', [Validators.required]],
     level: [''],
-    text: ['', [Validators.required, Validators.maxLength(1000)]],
-    file: ['']
+    text: ['', [Validators.required, Validators.maxLength(1000)]]
   });
   ngOnInit() {
+  }
+  onFileSelect(file) {
+    if (file.target.files[0]) {
+      this.slectedFile = file.target.files[0] as File;
+      const reader = new FileReader();
+      reader.readAsDataURL(this.slectedFile);
+      this.selected = true;
+      this.fileName = this.slectedFile.name;
+    }
   }
   onClear() {
     this.ticketForm.reset();
     this.matdialogRef.close();
+    this.slectedFile = null;
   }
   onSubmitCreate() {
     if (this.ticketForm.valid) {
-      this.ticketService.addTicket(this.ticketForm.value, this.authService.decodedToken.nameid).subscribe((data) => {
+      const ticket = new FormData();
+      // document = Object.assign({}, this.docLeftForm.value);
+      if (this.slectedFile !== null) {
+        ticket.append('file', this.slectedFile, this.slectedFile.name);
+      }
+      ticket.append('title', this.ticketForm.get('title').value);
+      ticket.append('department', this.ticketForm.get('department').value);
+      ticket.append('level', this.ticketForm.get('level').value);
+      ticket.append('text', this.ticketForm.get('text').value);
+      this.ticketService.addTicket(ticket, this.authService.decodedToken.nameid).subscribe((data) => {
         this.alertService.success('تیکتی شما با موفقیت ثبت شد', 'موفق');
         this.onClear();
         this.newTicket.emit(data);
