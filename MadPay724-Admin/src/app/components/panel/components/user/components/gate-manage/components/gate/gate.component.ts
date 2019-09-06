23961058e1b1dff321e7913a35c9efe7ec3dfc5c
Key1-lab <key.one72@gmail.com>
2019-09-06 17:48:25 +0430

    S25-E09

--- MadPay724-Admin/src/app/components/panel/components/user/components/gate-manage/components/gate/gate.component.ts
+++ MadPay724-Admin/src/app/components/panel/components/user/components/gate-manage/components/gate/gate.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Gate } from 'src/app/models/user/gate';
+import { Wallet } from 'src/app/models/wallet';
 
 @Component({
   selector: 'app-gate',
@@ -8,9 +9,12 @@ import { Gate } from 'src/app/models/user/gate';
 })
 export class GateComponent implements OnInit {
   @Input() gate: Gate;
+  @Input() wallets: Wallet[];
   constructor() { }
 
   ngOnInit() {
   }
-
+  getGateWallet(id: string): Wallet {
+    return this.wallets.find(p => p.id === id);
+}
 }
