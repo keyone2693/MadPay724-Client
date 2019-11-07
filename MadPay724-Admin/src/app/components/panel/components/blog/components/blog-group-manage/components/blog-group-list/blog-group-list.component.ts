537b50c6afe1486f3d82c05115bc5af69e216d52
Key1-lab <key.one72@gmail.com>
2019-11-07 16:42:14 +0330

    S28-E15

--- MadPay724-Admin/src/app/components/panel/components/blog/components/blog-group-manage/components/blog-group-list/blog-group-list.component.ts
+++ MadPay724-Admin/src/app/components/panel/components/blog/components/blog-group-manage/components/blog-group-list/blog-group-list.component.ts
@@ -1,18 +1,19 @@
-import {  OnInit, ViewChild, Component } from '@angular/core';
+import { OnInit, ViewChild, Component, OnDestroy } from '@angular/core';
 import { BlogGroup } from 'src/app/models/blog/blogGroup';
 import { MatTableDataSource, MatSort, MatPaginator } from '@angular/material';
 import { ToastrService } from 'ngx-toastr';
 import { BlogGroupService } from 'src/app/Services/panel/blog/blogGroup.service';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/Services/auth/auth.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-blog-group-list',
   templateUrl: './blog-group-list.component.html',
   styleUrls: ['./blog-group-list.component.css']
 })
-export class BlogGroupListComponent implements OnInit {
-
+export class BlogGroupListComponent implements OnInit, OnDestroy {
+  subManager = new Subscription();
   blogGroups: MatTableDataSource<BlogGroup>;
   blogGroupsArray: BlogGroup[];
   displayedColumns: string[] = ['id', 'name', 'price', 'actions'];
@@ -22,34 +23,40 @@ export class BlogGroupListComponent implements OnInit {
   loadingHideFlag = false;
   noContentHideFlag = true;
   constructor(private blogGroupService: BlogGroupService, private authService: AuthService,
-              private router: Router,
-              private alertService: ToastrService) { }
+    private router: Router,
+    private alertService: ToastrService) { }
 
   ngOnInit() {
     this.loadBlogGroups();
   }
+  ngOnDestroy() {
+    this.subManager.unsubscribe();
+  }
   loadBlogGroups() {
-    this.blogGroupService.getBlogGroups(this.authService.decodedToken.nameid).subscribe((data) => {
-      this.blogGroups = new MatTableDataSource(data);
-      this.blogGroupsArray = data;
-      this.blogGroups.sort = this.sort;
-      this.blogGroups.paginator = this.paginator;
-      this.loadingHideFlag = true;
-      if (data.length === 0) {
-        this.noContentHideFlag = false;
+    this.subManager.add(
+      this.blogGroupService.getBlogGroups(this.authService.decodedToken.nameid).subscribe((data) => {
+        this.blogGroups = new MatTableDataSource(data);
+        this.blogGroupsArray = data;
+        this.blogGroups.sort = this.sort;
+        this.blogGroups.paginator = this.paginator;
+        this.loadingHideFlag = true;
+        if (data.length === 0) {
+          this.noContentHideFlag = false;
+        }
+      }, error => {
+        this.alertService.error(error, 'خطا');
+        this.loadingHideFlag = false;
       }
-    }, error => {
-      this.alertService.error(error, 'خطا');
-      this.loadingHideFlag = false;
-    }
+      )
     );
+
   }
   onSearchClear() {
     this.searchKey = '';
     this.applyFilter();
   }
   applyFilter() {
-      this.blogGroups.filter = this.searchKey.trim();
+    this.blogGroups.filter = this.searchKey.trim();
   }
   getBlogGroupById(blogGroupId: string): string {
     const bg = this.blogGroupsArray.find(p => p.id === blogGroupId);
@@ -60,16 +67,19 @@ export class BlogGroupListComponent implements OnInit {
     }
   }
   onCreate() {
-      this.router.navigate(['/panel/blog/bloggroup/add']);
+    this.router.navigate(['/panel/blog/bloggroup/add']);
   }
   onDelete(blogGroup: BlogGroup) {
-    this.blogGroupService.deleteBlogGroup(this.authService.decodedToken.nameid, blogGroup.id).subscribe(() => {
-      this.alertService.success('دسته بندی بلاگ مورد نظر ب موفقیت حذف شد', 'موفق');
-      this.blogGroups.data.splice(this.blogGroups.data.indexOf(blogGroup), 1);
-      this.blogGroups._updateChangeSubscription();
-    }, error => {
-      this.alertService.error(error, 'خطا در حذف دسته بندی بلاگ');
-    });
+    this.subManager.add(
+      this.blogGroupService.deleteBlogGroup(this.authService.decodedToken.nameid, blogGroup.id).subscribe(() => {
+        this.alertService.success('دسته بندی بلاگ مورد نظر با موفقیت حذف شد', 'موفق');
+        this.blogGroups.data.splice(this.blogGroups.data.indexOf(blogGroup), 1);
+        this.blogGroups._updateChangeSubscription();
+      }, error => {
+        this.alertService.error(error, 'خطا در حذف دسته بندی بلاگ');
+      })
+    );
+
   }
 
 
