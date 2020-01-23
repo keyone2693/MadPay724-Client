b780acd58f7c775cfd09d12c4d0c9cdd353e166f
key.one72@gmail.com <key.one72@gmail.com>
2020-01-23 16:03:40 +0330

    S43-E02

--- MadPay724-Admin/src/app/Shared/Modules/textEditor/sharedEJTextEditor.module.ts
+++ MadPay724-Admin/src/app/Shared/Modules/textEditor/sharedEJTextEditor.module.ts
@@ -1,39 +1,41 @@
 import { NgModule } from '@angular/core';
-import { SwitchModule } from '@syncfusion/ej2-angular-buttons';
-import { TabModule } from '@syncfusion/ej2-angular-navigations';
-import { ButtonModule } from '@syncfusion/ej2-angular-buttons';
-import { DropDownListModule } from '@syncfusion/ej2-angular-dropdowns';
-import { DialogModule } from '@syncfusion/ej2-angular-popups';
-import { TextBoxModule } from '@syncfusion/ej2-angular-inputs';
-import { NumericTextBoxModule } from '@syncfusion/ej2-angular-inputs';
-import { RadioButtonModule } from '@syncfusion/ej2-angular-buttons';
-import { CheckBoxModule } from '@syncfusion/ej2-angular-buttons';
+// import { SwitchModule } from '@syncfusion/ej2-angular-buttons';
+// import { TabModule } from '@syncfusion/ej2-angular-navigations';
+// import { ButtonModule } from '@syncfusion/ej2-angular-buttons';
+// import { DropDownListModule } from '@syncfusion/ej2-angular-dropdowns';
+// import { DialogModule } from '@syncfusion/ej2-angular-popups';
+// import { TextBoxModule } from '@syncfusion/ej2-angular-inputs';
+// import { NumericTextBoxModule } from '@syncfusion/ej2-angular-inputs';
+// import { RadioButtonModule } from '@syncfusion/ej2-angular-buttons';
+// import { CheckBoxModule } from '@syncfusion/ej2-angular-buttons';
 import { RichTextEditorAllModule } from '@syncfusion/ej2-angular-richtexteditor';
 
 @NgModule({
   imports: [
-    TabModule,
+    
     RichTextEditorAllModule,
-    CheckBoxModule,
-    DialogModule,
-    NumericTextBoxModule,
-    ButtonModule,
-    SwitchModule,
-    RadioButtonModule,
-    TextBoxModule,
-    DropDownListModule
+    // TabModule,
+    // CheckBoxModule,
+    // DialogModule,
+    // NumericTextBoxModule,
+    // ButtonModule,
+    // SwitchModule,
+    // RadioButtonModule,
+    // TextBoxModule,
+    // DropDownListModule
   ],
   exports: [
-    TabModule,
+   
     RichTextEditorAllModule,
-    CheckBoxModule,
-    DialogModule,
-    NumericTextBoxModule,
-    ButtonModule,
-    SwitchModule,
-    RadioButtonModule,
-    TextBoxModule,
-    DropDownListModule
+    // TabModule,
+    // CheckBoxModule,
+    // DialogModule,
+    // NumericTextBoxModule,
+    // ButtonModule,
+    // SwitchModule,
+    // RadioButtonModule,
+    // TextBoxModule,
+    // DropDownListModule
   ],
 })
 export class SharedEJTextEditorModule { }
