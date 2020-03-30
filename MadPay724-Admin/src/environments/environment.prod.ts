b5c9f70dfb3a30be63b49458eaffd36b3cf4f3c0
key.one72@gmail.com <key.one72@gmail.com>
2020-03-30 19:31:51 +0430

    S51-E01

--- MadPay724-Admin/src/environments/environment.prod.ts
+++ MadPay724-Admin/src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   development: true,
   production: true,
-  apiUrl: 'https://localhost:44342/',
-  apiUrlJwt: 'localhost:44342',
+  apiUrl: 'https://localhost:44386/',
+  apiUrlJwt: 'localhost:44386',
   apiV1: 'api/v1/',
   encryptSecretKey: '@ot0"hreOi"@=OA',
   expireHour: 24,
--- MadPay724-App/src/environments/environment.prod.ts
+++ MadPay724-App/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://localhost:44342/',
+  apiUrl: 'https://localhost:44386/',
   apiV1: 'api/v1/',
 };
